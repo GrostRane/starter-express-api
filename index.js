a1963b3c39433dc38df6789cf271f52fc6fdd837
GrostRane <79666398+GrostRane@users.noreply.github.com>
2023-02-17 07:23:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+app.all('/', async function (req, res) => {
+    
+  let Opensea = `https://api.opensea.io/api/v1/collections?asset_owner=0x4ddf892e0051C0F20a97f6E6c1c4A8868e95A971&format=json`;
+
+  const response = await axios.get(Opensea);
+  const response2 =response.data
+  console.log(response2)
+    
     console.log("Just got a request!")
-    res.send('Yodd!')
+    res.send({response2})
 })
 app.listen(process.env.PORT || 3000)
